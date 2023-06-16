4ff0a6da5cad7b1519097499a13db886fbbfeea9
lightCode1840 <1840527533@qq.com>
2023-06-16 17:36:19 +0800

    初始化springboot项目

---
+++ src/main/java/com/example/gitgit/GitgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitgitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitgit/GitgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
