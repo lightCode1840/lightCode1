e964a4ccdbdd3cd9a509c40fc3cef74ea049a970
lightCode1840 <1840527533@qq.com>
2023-06-16 17:51:36 +0800

    增加HelloController并增加hello方法，

---
+++ src/main/java/com/example/gitgit/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.gitgit.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lilepingstart
+ * @creat 2023-06-16 17:49
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("hello");
+        return "helloString";
+    }
+}
